ee0800f45b4fb489334ec309a0bc8e47badb7d5e
samax-dx <samax.dx@aol.com>
2021-10-10 12:03:35 +0600

    CategoryList updated

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,14 +1,30 @@
-import { Table } from "antd";
+import { Table, Button } from "antd";
+
+const CategoryActions = ({ categoryId }) => {
+    return (
+        <div>
+            <Button type="link" title="delete" />
+        </div>
+    );
+};
 
 export const CategoryList = props => {
     const categories = ["Food", "Cosmetics", "Electronics", "Sports"].map(
-        (cat, i) => ({ id: ++i, name: cat, key: i, productCount: i % 2 === 0 ? i * 2 : i * 3 })
+        (cat, i) => ({ id: ++i, name: cat, productCount: i % 2 === 0 ? i * 2 : i * 3 })
     );
 
     return (
-        <Table dataSource={categories} key="id">
+        <Table
+            dataSource={categories}
+            rowKey="id"
+            onRow={(data, index) => ({
+                onClick: e => console.log(e)
+            })}
+            pagination={{pageSize: 3, current: 1}}
+        >
             <Table.Column title="Product Categories" dataIndex="name" />
             <Table.Column title="Product Count" dataIndex="productCount" />
+            <Table.Column title="Actions" children={<CategoryActions />}/>
         </Table>
     );
 };
