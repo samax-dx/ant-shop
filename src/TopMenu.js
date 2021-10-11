17f64ef797d3acff072d43d75cbafa83b524ca3c
samax-dx <samax.dx@aol.com>
2021-10-11 21:27:15 +0600

    updated formatting

--- src/TopMenu.js
+++ src/TopMenu.js
@@ -1,4 +1,4 @@
-import {Menu} from "antd";
+import { Menu } from "antd";
 import React from "react";
 import { useActor } from '@xstate/react';
 import { menuMachine } from './machines/menuMachine';
@@ -7,8 +7,8 @@ export function TopMenu() {
     const [current, send] = useActor(menuMachine);
 
     return <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["home"]}>
-        <Menu.Item key="home" onClick={()=>send({ type: 'NAV_HOME' })}>Home</Menu.Item>
-        <Menu.Item key="product" onClick={()=>send({ type: 'NAV_PRODUCT' })}>Products</Menu.Item>
-        <Menu.Item key="category" onClick={()=>send({ type: 'NAV_CATEGORY' })}>Categories</Menu.Item>
+        <Menu.Item key="home" onClick={() => send({ type: 'NAV_HOME' })}>Home</Menu.Item>
+        <Menu.Item key="product" onClick={() => send({ type: 'NAV_PRODUCT' })}>Products</Menu.Item>
+        <Menu.Item key="category" onClick={() => send({ type: 'NAV_CATEGORY' })}>Categories</Menu.Item>
     </Menu>;
 }
