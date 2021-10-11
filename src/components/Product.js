548df36a80dba9c93256be41f5ed4fafcc57810b
samax-dx <samax.dx@aol.com>
2021-10-11 22:02:24 +0600

    added dummy products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,18 @@
+import { Table } from "antd"
+import products from './products.json';
+
 export const Product = props => {
     return (
-        <h2>Page: Product</h2>
+        <Table
+            dataSource={products}
+            rowKey="id"
+            onRow={(data, index) => ({
+                onClick: e => console.log(e)
+            })}
+            pagination={{ pageSize: 3 }}
+        >
+            <Table.Column title="Product" dataIndex="name" />
+            <Table.Column title="Category" dataIndex="category" />
+        </Table>
     );
 };
