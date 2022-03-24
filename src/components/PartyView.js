7306a37228114f91721bc142828dba569b44c3db
samax-dx <samax.dx@aol.com>
2022-03-24 15:52:31 +0600

    Essential party management functional

--- src/components/PartyView.js
+++ src/components/PartyView.js
@@ -9,27 +9,6 @@ export const PartyView = ({ actor }) => {
     const { party } = vmState.context;
     const onClose = () => sendParent({ type: "VIEW_LIST" });
 
-    // return (
-    //     <Modal
-    //         title={`Party : ${party.username}`}
-    //         visible={true}
-    //         onCancel={onClose}
-    //         footer={[
-    //             <Button type="primary" key="btnSave" onClick={() => sendParent({ type: "EDIT_ITEM", data: party })}>Edit</Button>,
-    //             <Button type="primary" danger key="btnClose" onClick={onClose}>Close</Button>
-    //         ]}
-    //     >
-    //         <Table
-    //             size="small"
-    //             dataSource={Object.entries(party)}
-    //             rowKey={0}
-    //             pagination={false}
-    //         >
-    //             <Table.Column title={"Field"} dataIndex={0}></Table.Column>
-    //             <Table.Column title={"Value"} dataIndex={1}></Table.Column>
-    //         </Table>
-    //     </Modal>
-    // );
     return (
         <Modal
             title={`Party : ${party.username}`}
